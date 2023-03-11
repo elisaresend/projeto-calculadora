38f260d49c68b9e50cb1613eb4937ddf554e93e0
Elisabete Resende <182elisa182@gmail.com>
2023-03-11 12:46:14 -0300

    first commit

--- src/br/com/teste/calculadora/Calculadora.java
+++ src/br/com/teste/calculadora/Calculadora.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int a,b;		
-		System.out.printf("Digite o primeiro valor: ");
-		a = scan.nextInt();
-		System.out.printf("Digite o segundo valor: ");
-		b = scan.nextInt();
-		
-		int soma = soma(a,b);
-		int subtracao = subtracao(a,b);
-		int multiplicacao = multiplicacao(a,b);		
-		double divisao = divisao(a,b);
+	public static void main(String[]args) {
+		try (Scanner scan = new Scanner(System.in)) {
+			int a,b;		
+			System.out.println("Digite o primeiro valor: ");
+			a = scan.nextInt();
+			System.out.println("Digite o segundo valor: ");
+			b = scan.nextInt();
 			
-		System.out.printf(String.format("O resultado da soma é: %d \n", soma));
-		System.out.printf(String.format("O resultado da subtração é: %d \n", subtracao));
-		System.out.printf(String.format("O resultado da divisão é: %.2f \n", divisao));
-		System.out.printf(String.format("O resultado da multiplicacao é: %d \n", multiplicacao));
+			int soma = soma(a,b);
+			int subtracao = subtracao(a,b);
+			int multiplicacao = multiplicacao(a,b);		
+			double divisao = divisao(a,b);
+				
+			System.out.println(String.format("O resultado da soma é: %d \n", soma));
+			System.out.println(String.format("O resultado da subtração é: %d \n", subtracao));
+			System.out.println(String.format("O resultado da divisão é: %.2f \n", divisao));
+			System.out.println(String.format("O resultado da multiplicacao é: %d \n", multiplicacao));
+		}
 	}
-
+	
 	public static int soma(int a, int b) {
 		return a + b;
 	}
